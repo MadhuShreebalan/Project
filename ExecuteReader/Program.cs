d7c51de667c64b01bf4790ec878b0219aa7005fb
MadhuShreebalan <42761089+MadhuShreebalan@users.noreply.github.com>
2020-01-24 16:09:37 +0530

    Add files via upload

---
+++ ExecuteReader/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace ExecuteReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string data = "datasource = .; database= Details; Intergrated security = SSPI";
+            using (SqlConnection connection = new SqlConnection("data source = .; database= Details; Integrated security = SSPI"))
+            {
+                // command.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlCommand command = new SqlCommand("select MAX(SOLD) from  Software", connection);
+                connection.Open();
+                /* using data reader function:
+                 using (SqlDataReader sqlDataReader = command.ExecuteReader())
+                {
+
+                    while (sqlDataReader.Read())
+                    {
+                        Console.WriteLine("{0}", sqlDataReader[0]);
+
+                    }
+                }
+                */
+
+                //DataAdapter:
+              
+
+            }
+        }
+    }
+}
